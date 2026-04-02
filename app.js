367caf0379c64b48fd8d45d6ac5ac15403ab3f4f
Roberto Gonzalez <rob.gonzalez.castro@gmail.com>
2026-04-02 19:08:36 -0400

    fix: agregando cors para POST nueva tarjeta

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
